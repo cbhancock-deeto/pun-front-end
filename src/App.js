240f42b7135efb605e1bf115a367d2fe7afbf25c
cody <cblainehancock@gmail.com>
2020-11-30 14:33:24 -0700

    finished front-end

--- src/App.js
+++ src/App.js
@@ -1,50 +1,68 @@
-import React, { useState } from "react";
-import JokeSuggestion from "./components/JokeSuggestion";
-import { Grid, Button } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import {
+	Grid,
+	Button,
+	Typography,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+	heading: {
+		fontWeight: "600",
+		paddingBottom: 70,
+		fontSize: "2rem",
+	},
+	container: {
+		backgroundColor: "#e7dfd5",
+		fontFamily: "Verdana, Geneva, sans-serif",
+		top: "200",
+		textAlign: "center",
+		height: "100vh",
+		margin: 0,
+	},
+	joke: { paddingTop: 80 },
+}));
 
 function App() {
-	const [randomJoke, setRandomJoke] = useState("testy");
+	const [randomJoke, setRandomJoke] = useState("");
+	const [jokeArr, setJokeArr] = useState([]);
+
+	useEffect(() => {
+		const apiURL =
+			"https://calm-stream-56867.herokuapp.com/jokes";
+		fetch(apiURL)
+			.then(response => response.json())
+			.then(data => setJokeArr(data));
+	}, []);
 
 	const randomJokeHandler = () => {
-		let testText = "a random joke";
-		setRandomJoke(testText);
+		let randVal = Math.random() * jokeArr.length;
+		let wholeRandom = Math.floor(randVal);
+		setRandomJoke(jokeArr[wholeRandom].joke);
 	};
 
-	// const getAJoke = () => {
-	// 	fetch("https://example.com/profile", {
-	// 		method: "POST", // or 'PUT'
-	// 		headers: {
-	// 			"Content-Type": "application/json",
-	// 		},
-	// 		body: JSON.stringify(data),
-	// 	})
-	// 		.then(response => response.json())
-	// 		.then(data => {
-	// 			console.log("Success:", data);
-	// 		})
-	// 		.catch(error => {
-	// 			console.error("Error:", error);
-	// 		});
-	// };
-
+	const classes = useStyles();
 	return (
 		<Grid
 			container
+			className={classes.container}
 			direction='column'
 			alignItems='center'
-			spacing={3}>
-			<Grid item>Welcome to the Punny API!</Grid>
-			<Grid item>
-				<Button variant='contained'>Suggest a Joke</Button>
+			justify='center'>
+			<Grid item className={classes.heading}>
+				Welcome to the Punny API!
 			</Grid>
 			<Grid item>
 				<Button
 					variant='contained'
-					onClick={randomJokeHandler}>
+					onClick={randomJokeHandler}
+					className={classes.button}>
 					Random Joke
 				</Button>
 			</Grid>
-			<Grid item>{randomJoke}</Grid>
+			<Grid item className={classes.joke}>
+				<Typography>{randomJoke}</Typography>
+			</Grid>
 		</Grid>
 	);
 }
--- src/components/JokeSuggestion.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function JokeSuggestion() {
-	return <div>This the JokeSuggestion screen!</div>;
-}
-
-export default JokeSuggestion;
